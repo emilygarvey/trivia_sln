656a5b19fb509a8039cdac4faafbbacaf50c5e87
Emily Garvey <41160828+emilygarvey@users.noreply.github.com>
2020-12-16 14:44:32 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,62 @@
+import React, { Component, useState } from "react";
+import "../css/App.css";
+import data from "../sample_data.json";
+// import components here
+
+function App() {
+  const [answerState, setAnswerState] = useState("unanswered");
+  const [currentQuestionNum, setCurrentQuestionNum] = useState(0);
+
+  //function handleClick() {}
+
+  return (
+    <div className="app">
+      <h1>Trivia!</h1>
+      {/* <p>Click the correct answer choice below...</p> */}
+      <Question
+        question={data[currentQuestionNum].question.text}
+        choices={data[currentQuestionNum].question.choices}
+      />
+
+      <button
+        onClick={() =>
+          setAnswerState(
+            data[currentQuestionNum].question.choices[
+              data[currentQuestionNum].question.correct_choice_index
+            ]
+          )
+        }
+      >
+        Click for the correct answer
+      </button>
+      <p>The correct answer is {answerState}</p>
+
+      <NextQuestion
+        goToNextQuestion={() => setCurrentQuestionNum(currentQuestionNum + 1)}
+        resetAnswerState={() => setAnswerState("unanswered")}
+      />
+    </div>
+  );
+}
+
+function Question(props) {
+  return (
+    <div>
+      {props.question}
+      <Answer answer={props.choices[0]} />
+      <Answer answer={props.choices[1]} />
+      <Answer answer={props.choices[2]} />
+      <Answer answer={props.choices[3]} />
+    </div>
+  );
+}
+
+function Answer(props) {
+  return <div>{props.answer}</div>;
+}
+
+function NextQuestion(props) {
+  return <button onClick={props.goToNextQuestion}>Next Question</button>;
+}
+
+export default App;
---
+++ src/components/Question.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// import components
+
+// - [ ] Create the Question class to store the data from the sample objects.
+// - [ ] Style the component in any way you choose.
+
+function Question() {
+  return <div>{data.question.question_text}</div>;
+}
+
+export default Question;
